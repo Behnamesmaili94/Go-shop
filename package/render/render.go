97d69963b975150960e456ee8074da83a3c9488a
BehnamEsmaili <58373858+Behnamesmaili94@users.noreply.github.com>
2023-06-08 13:02:16 +0330

    Create render.go

---
+++ package/render/render.go
@@ -0,0 +1,17 @@
+package render
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+// this function is doing render html files in template directory
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	parsTemplat, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsTemplat.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error has been reached", err)
+
+	}
+}
